279215b1f9fba240f4a6cdc0428746deb87d5bc2
Kirk Ke Tang <nus.thomaskirk@gmail.com>
2015-09-02 14:49:34 -0700

    empty-playground and predefs

---
+++ predefs.js
@@ -0,0 +1,5 @@
+var callback=function(err, data) {
+	console.log('callback err, data from console', err, data);
+};
+var options = {};
+branch.init('key_live_gnbMtci1g3qBX2jRNsF1RhjpswaZkiaa', options, callback);

6d008503787a66b1ca57d23bf21a9c23c7331bfc
Kirk Ke Tang <nus.thomaskirk@gmail.com>
2015-09-03 15:45:59 -0700

    include predefs in empty-playground

--- predefs.js
+++ predefs.js
@@ -1,5 +1,13 @@
-var callback=function(err, data) {
+var callbackerrdata=function(data) {
+	console.log('callback data from console', data);
+};
+var callbackerr=function(err) {
+	console.log('callback err from console', err);
+};
+var callbackerrdata=function(err, data) {
 	console.log('callback err, data from console', err, data);
 };
 var options = {};
-branch.init('key_live_gnbMtci1g3qBX2jRNsF1RhjpswaZkiaa', options, callback);
+var init = function() {
+	branch.init('key_live_gnbMtci1g3qBX2jRNsF1RhjpswaZkiaa', options, callbackerrdata);
+};

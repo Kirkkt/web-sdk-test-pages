428e28e514ee081d15912c3268056472040839ac
Kirk Ke Tang <nus.thomaskirk@gmail.com>
2015-09-03 16:04:18 -0700

    new setIdentity-logout-error on minified js

--- predefs.js
+++ predefs.js
@@ -1,12 +1,15 @@
-var callbackerrdata=function(data) {
+var callbackdata=function(data) {
 	console.log('callback data from console', data);
 };
+var cbd = callbackdata;
 var callbackerr=function(err) {
 	console.log('callback err from console', err);
 };
+var cbe = callbackerr;
 var callbackerrdata=function(err, data) {
 	console.log('callback err, data from console', err, data);
 };
+var cbed = callbackerrdata;
 var options = {};
 var init = function() {
 	branch.init('key_live_gnbMtci1g3qBX2jRNsF1RhjpswaZkiaa', options, callbackerrdata);

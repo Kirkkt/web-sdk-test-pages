5b81dca1c4951837ef96383d1ed290361210565a
Kirk Ke Tang <nus.thomaskirk@gmail.com>
2015-10-23 08:37:25 -0700

    before moving include into predefs

--- predefs.js
+++ predefs.js
@@ -1,16 +1,28 @@
+var localkey = 'key_live_ggkIIzvaEwblwYQp7ATCMomepdfePSjU';
+
 var callbackdata=function(data) {
 	console.log('callback data from console', data);
 };
 var cbd = callbackdata;
+
 var callbackerr=function(err) {
 	console.log('callback err from console', err);
 };
 var cbe = callbackerr;
+
 var callbackerrdata=function(err, data) {
 	console.log('callback err, data from console', err, data);
 };
 var cbed = callbackerrdata;
+
 var options = {};
+var data = {};
+var linkData = {};
+
 var init = function() {
-	branch.init('key_live_gnbMtci1g3qBX2jRNsF1RhjpswaZkiaa', options, callbackerrdata);
+	branch.init('key_live_gnbMtci1g3qBX2jRNsF1RhjpswaZkiaa', options, function(err, data) {console.log("callback after init:", err, data);});
+};
+
+var localinit = function() {
+	branch.init(localkey, options, function(err, data) {console.log("callback after init:", err, data);});
 };
